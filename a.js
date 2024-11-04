88df3af31da0e45aaaf1a3d9ee0e93759dd3d217
Yu Zhang <yu.zhang.yz862@yale.edu>
2024-11-04 16:27:01 +0800

    refactor structure

--- a.js
+++ a.js
@@ -1,4 +1,20 @@
-const { instance } = await WebAssembly.instantiateStreaming(fetch(new URL('./target/wasm-gc/release/build/hello.wasm', import.meta.url)), {}, { builtins: ['js-string'], importedStringConstants: "moonbit:constant_strings" });
+const { instance } = await WebAssembly.instantiateStreaming(
+  fetch(
+    new URL(
+      "./target/wasm-gc/release/build/palindrome/palindrome.wasm",
+      import.meta.url
+    )
+  ),
+  {
+    console: {
+      log: console.log,
+    },
+  },
+  {
+    builtins: ["js-string"],
+    importedStringConstants: "moonbit:constant_strings",
+  }
+);
 
 console.log(instance.exports.palindrome("hello"));
 console.log(instance.exports.palindrome("ada"));
--- b.mjs
+++ b.mjs
@@ -1,10 +1,19 @@
-const { instance } = await WebAssembly.instantiateStreaming(fetch(new URL('./target/wasm-gc/release/build/demo-js-builtin-string.wasm', import.meta.url)), {
-  dom: {
-    set_css: (dom, key, value) => {
-      dom.style[key] = value;
-    }
+const { instance } = await WebAssembly.instantiateStreaming(
+  fetch(
+    new URL("./target/wasm-gc/release/build/dom/dom.wasm", import.meta.url)
+  ),
+  {
+    dom: {
+      set_css: (dom, key, value) => {
+        dom.style[key] = value;
+      },
+    },
+  },
+  {
+    builtins: ["js-string"],
+    importedStringConstants: "moonbit:constant_strings",
   }
-}, { builtins: ['js-string'], importedStringConstants: "moonbit:constant_strings" });
+);
 
 document.getElementById("hello").addEventListener("click", () => {
   instance.exports.change_color(document.getElementById("hello"));
